77776ddf66d0291372eda3a1a6082680586c54d9
Kyle <KyleMit@gmail.com>
2021-10-21 22:48:05 -0400

    add array manipulation

---
+++ challenges/array-manipulation.ts
@@ -0,0 +1,27 @@
+export function main() {
+    const n = 5
+    const queries = [
+        [ 1, 2, 100 ],
+        [ 2, 5, 100 ],[ 3, 4, 100 ]
+    ]
+    const result = arrayManipulation(n, queries);
+    const expected = 200
+    console.log({result, expected});
+}
+
+/*
+ * Complete the 'arrayManipulation' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function arrayManipulation(n: number, queries: number[][]): number {
+    // Write your code here
+
+}
+
+main()
+
