47bc638186a299bf1bafd982fccbfd8b766c2662
Kyle <KyleMit@gmail.com>
2021-10-21 20:40:34 -0400

    2d array setup

---
+++ challenges/2d-array.ts
@@ -0,0 +1,29 @@
+main();
+
+function main() {
+    let arr = [
+        [1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0],
+        [0, 0, 2, 4, 4, 0],
+        [0, 0, 0, 2, 0, 0],
+        [0, 0, 1, 2, 4, 0]
+    ];
+
+    const result: number = hourglassSum(arr);
+
+    console.log(result)
+}
+
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function hourglassSum(arr: number[][]): number {
+    // Write your code here
+
+}
