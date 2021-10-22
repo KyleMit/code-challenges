268453a2be2dc204c02c1a3f2ae59060eafa8361
Kyle <KyleMit@gmail.com>
2021-10-21 22:24:38 -0400

    add rotate left challange

---
+++ challenges/rotate-left.ts
@@ -0,0 +1,24 @@
+export function main() {
+    const d = 4
+    const arr = [ 1, 2, 3, 4, 5 ]
+    const result: number[] = rotateLeft(d, arr);
+    console.log(result)
+}
+
+/*
+ * Complete the 'rotateLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+function rotateLeft(d: number, arr: number[]): number[] {
+
+
+}
+
+
+
+main();
