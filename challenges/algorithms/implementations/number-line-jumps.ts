ede7fb7df017db3737df4a79021719a66ce5508d
Kyle <KyleMit@gmail.com>
2021-10-25 23:17:23 -0400

    solve number of jumps

--- challenges/algorithms/implementations/number-line-jumps.ts
+++ challenges/algorithms/implementations/number-line-jumps.ts
@@ -15,11 +15,16 @@ export function main() {
  */
 
 function kangaroo(x1: number, v1: number, x2: number, v2: number): string {
-    let i = 0
-    let pos1 = x1 + v1 * i
-    let pos2 = x2 + v2 * i
-    let response = pos1 == pos2 ? "YES" : "NO";
-    return response;
+    // velocity must be an increment of start delta
+    const startDelta = Math.abs(x1 - x2);   // | 0 - 4 | = 4
+    const velocityDelta = Math.abs(v1 - v2) // | 3 - 2 | = 1
+    const velocityIsDivisible = startDelta % velocityDelta === 0
+
+    // kangaroos must start in the correct position
+    const inCorrectOrder = (x1 < x2 && v1 > v2) // behind and faster
+                        || (x1 > x2 && v1 < v2) // ahead and slower
+
+    return inCorrectOrder && velocityIsDivisible ? "YES" : "NO";
 }
 
 main()
