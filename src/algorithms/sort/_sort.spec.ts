37f54948544453dc0db784518391312ba0aa9ef7
Kyle <KyleMit@gmail.com>
2021-11-07 18:29:57 -0500

    add tests

---
+++ src/algorithms/sort/_sort.spec.ts
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import { bubbleSort } from './bubble-sort'
+import { insertionSort } from './insertion-sort'
+import { mergeSort } from './merge-sort'
+import { quickSort } from './quick-sort'
+import { selectionSort } from './selection-sort'
+
+const simple = (fn: (arr: any[]) => any[]) => {
+    const input = [1,3,2];
+    const output = fn(input);
+    const expected = [1,2,3]
+    expect(output).to.deep.equal(expected);
+}
+
+describe("bubble sort", function() {
+    it('should return simple array in order', function() {
+        simple(bubbleSort)
+    })
+})
+
+describe("insertion sort", function() {
+    it('should return simple array in order', function() {
+        simple(insertionSort)
+    })
+})
+
+describe("merge sort", function() {
+    it('should return simple array in order', function() {
+        simple(mergeSort)
+    })
+})
+
+describe("quick sort", function() {
+    it('should return simple array in order', function() {
+        simple(quickSort)
+    })
+})
+
+describe("selection sort", function() {
+    it('should return simple array in order', function() {
+        simple(selectionSort)
+    })
+})
