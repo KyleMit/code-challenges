a7baa941602c6fec6f2e419ba403798a670f69d6
KyleMit <kmitofsky@stackoverflow.com>
2022-06-10 10:07:14 -0400

    leet code - data structures I - day 3

---
+++ src/leet-code/data-structures/best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+
+// Time Complexity - O(n²)
+export function maxProfit(prices: number[]): number {
+    let maxDelta = 0;
+    for (let i=0; i < prices.length; i++) {
+        for (let j=i+1; j < prices.length; j++) {
+            const curDelta = prices[j] - prices[i]
+            if (curDelta > maxDelta) {
+                maxDelta = curDelta
+            }
+        }
+    }
+    return maxDelta
+};
+
+// Time Complexity - O(n)
+export function maxProfit2(prices: number[]): number {
+    let minValue = prices[0];
+    let maxDelta = 0;
+    for (let i=0; i < prices.length; i++) {
+        const value = prices[i];
+        const curDelta = value - minValue
+
+        if (value < minValue) {
+            // set new min value
+            minValue = value;
+        } else if (curDelta > maxDelta) {
+            // set new max delta
+            maxDelta = curDelta
+        }
+    }
+    return maxDelta
+};
+
+
+maxProfit([7,1,5,3,6,4]) // 5
+maxProfit([7,6,4,3,1]) // 0
---
+++ src/leet-code/data-structures/intersection-of-two-arrays-ii.ts
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+// Time Complexity: O(n + m)
+export function intersect(nums1: number[], nums2: number[]): number[] {
+    // get histogram of number distribution
+    const nums1Freq = nums1.reduce((acc, cur) => {
+        acc.set(cur, (acc.get(cur) ?? 0) + 1)
+        return acc
+    }, new Map())
+
+    // find matches in nums2
+    const result = nums2.reduce((acc, cur) => {
+        if (nums1Freq.get(cur) > 0) {
+            // got a hit, add to result and decrement
+            acc.push(cur)
+            nums1Freq.set(cur, nums1Freq.get(cur) - 1)
+        }
+        return acc
+    }, [] as number[])
+    return result
+};
+
+
+intersect([1,2,2,1], [2,1]) // [2,1]
